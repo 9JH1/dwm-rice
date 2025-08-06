8f46d40d6dcdc1b9340fb0423de45bf18ca7c5c4
= <tkf.x1os@gmail.com>
2025-08-07 11:45:14 +1200

    fixed autostart issue, sorry

--- src/autostart.sh
+++ src/autostart.sh
@@ -1,6 +1,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x1080 --rotate inverted &
-$SCRIPT_DIR/src/wal.sh  &
+$SCRIPT_DIR/wal.sh  &
 $HOME/Pictures/Wallpapers/.walltaker/walltaker.sh --id "46554" &
 nm-applet & 
 blueman-applet & 
