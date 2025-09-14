17c79a0a44acb2ec440511a20ec43d20f1e35e94
9jh1 <tkf.x1os@gmail.com>
2025-09-14 12:08:55 +1200

    base

--- src/playerctl.sh
+++ src/playerctl.sh
@@ -5,7 +5,7 @@ if [[ "$(playerctl metadata --format '-' 2>/dev/null)" == *-* ]]; then
 	raw_title=$(playerctl -a metadata --format '{{ title }}')
 
 	# Shorten to 10 chars max with ...
-	artist=$(echo "$raw_artist" | awk '{if(length > 20) printf "%.20s...\n", $0; else print}')
+	artist=$(echo "$raw_artist" | awk '{if(length > 15) printf "%.15s...\n", $0; else print}')
 	title=$(echo "$raw_title" | awk '{if(length > 20) printf "%.20s...\n", $0; else print}')
 
 	echo "$artist %{F$color0}-%{F$color7} %{T2}$title%{T-}"
