47a461dc8c4d934dc487e0eb931fbf23bd0ded06
= <=>
2025-12-12 18:41:25 +1300

    auto_dwm_Fri Dec 12 18:41:25 NZDT 2025

--- source/dwm/dwm.c
+++ source/dwm/dwm.c
@@ -1881,7 +1881,7 @@ setup(void)
 	if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
 	lrpad = drw->fonts->h;
-	bh = drw->fonts->h + 40;
+	bh = drw->fonts->h + 2;
 	updategeom();
 	/* init atoms */
 	utf8string = XInternAtom(dpy, "UTF8_STRING", False);
